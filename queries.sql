ff3d3cfdb6739b71c501f22788694c0aa803dc66
Khawlasidaoui <79415699+Khawlasidaoui@users.noreply.github.com>
2021-04-11 15:40:03 -0400

    updates

--- queries.sql
+++ queries.sql
@@ -22,3 +22,16 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info
+
+
+-- Remove retirement_info table and rewrite query to show emp_no column in addition to existing last_name and first_name
+DROP TABLE retirement_info;
+-- Add emp_no to query
+SELECT first_name, last_name, emp_no
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info
+
